c50b29ad499b9dbf1dcc249f6a8b3eb466ffe075
Flynn Duniho <flynn@harveyosity.org>
2021-04-17 23:42:59 -0700

    Merge from Flynn (#1)
    * Cleaned up project. TODO: Get the Autocrop working
    
    * Add readme and gitignore

---
+++ Pokedex/Converter/EnumColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Pokedex.Converter
+{
+    public class EnumColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter = null, CultureInfo culture = null)
+        {
+            if (value is Enum)
+            {
+                string s = value.ToString();
+                return (Color)Application.Current.Resources[value.ToString()];
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter = null, CultureInfo culture = null)
+        {
+            //if (value is int)
+            //{
+            //    return Enum.ToObject(targetType, value);
+            //}
+            return Binding.DoNothing;
+        }
+    }
+}
